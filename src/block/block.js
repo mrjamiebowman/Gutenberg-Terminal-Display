bbc2c44a726f67723325123c391c0959eea18476
Jamie Bowman <mrjamiebowman@gmail.com>
2019-11-17 00:55:24 -0500

    updates but a bug in validation.

--- src/block/block.js
+++ src/block/block.js
@@ -11,6 +11,7 @@ import './style.scss';
 
 const { __ } = wp.i18n; // Import __() from wp.i18n
 const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { RichText } = wp.editor;
 
 /**
  * Register: aa Gutenberg Block.
@@ -37,6 +38,13 @@ registerBlockType( 'mrjb/terminal-display', {
 		__( 'ParrotOS Terminal' ),
 	],
 
+	attributes: {
+		terminalData: {
+			source: 'html',
+			selector: 'p',
+		},
+	},
+
 	/**
 	 * The edit function describes the structure of your block in the context of the editor.
 	 * This represents what the editor will render when the block is used.
@@ -53,9 +61,9 @@ registerBlockType( 'mrjb/terminal-display', {
 		/**
 		 * Update content on change.
 		 */
-		function onChangeContent( newContent ) {
-			debugger;
-			props.setAttributes( { content: newContent } );
+
+		function onChangeTerminalData (value) {
+			props.setAttributes( { terminalData: value } );
 		}
 
 		// Creates a <p class='wp-block-mrjb-terminal-display'></p>.
@@ -63,19 +71,17 @@ registerBlockType( 'mrjb/terminal-display', {
 			<div className={ props.className }>
 				<div className="terminal">
 					<div className="command">
-						<span className="red">┌─[✗</span>]─[<span className="user">user</span><span className="at">@</span><span className="hostname">parrot</span><span className="red">]─[</span><span className="tilde">~</span><span class="red">]</span><br/>
+						<span className="red">┌─[✗</span>]─[<span className="user">user</span><span className="at">@</span><span className="hostname">parrot</span><span className="red">]─[</span><span className="tilde">~</span><span className="red">]</span><br/>
 						<span className="red">└──╼</span> <span className="at">$</span> <a href="javascript:none" title="Click to copy">sudo namp -sU 192.168.0.34</a>
 					</div>
-					<p>
-						20/tcp   closed ftp-data<br/>
-						21/tcp   open   ftp         vsftpd 2.0.8 or later<br/>
-						22/tcp   open   ssh         OpenSSH 7.2p2 Ubuntu 4 (Ubuntu Linux; protocol 2.0)<br/>
-						53/tcp   open   domain      dnsmasq 2.75<br/>
-						80/tcp   open   http        PHP cli server 5.5 or later<br/>
-						139/tcp  open   netbios-ssn Samba smbd 3.X - 4.X (workgroup: WORKGROUP)<br/>
-						666/tcp  open   doom?<br/>
-						3306/tcp open   mysql       MySQL 5.7.12-0ubuntu1<br/>
-					</p>
+					<RichText
+						format="string"
+						formattingControls={ [] }
+						placeholder={ __( 'Terminal Output' ) }
+						onChange={ onChangeTerminalData }
+						value={ props.attributes.terminalData }
+						tagName="p"
+					/>
 				</div>
 				<div className="info-tag"><a href="https://github.com/mrjamiebowman/Gutenberg-Terminal-Display" target="_blank">(Gutenberg Terminal Display)</a></div>
 			</div>
@@ -94,11 +100,12 @@ registerBlockType( 'mrjb/terminal-display', {
 	 * @returns {Mixed} JSX Frontend HTML.
 	 */
 	save: ( props ) => {
+
 		return (
 			<div className={ props.className }>
 				<div className="terminal">
 					<div className="command">
-						<span className="red">┌─[✗</span>]─[<span className="user">user</span><span className="at">@</span><span className="hostname">parrot</span><span className="red">]─[</span><span className="tilde">~</span><span class="red">]</span><br/>
+						<span className="red">┌─[✗</span>]─[<span className="user">user</span><span className="at">@</span><span className="hostname">parrot</span><span className="red">]─[</span><span className="tilde">~</span><span className="red">]</span><br/>
 						<span className="red">└──╼</span> <span className="at">$</span> <a href="javascript:none" title="Click to copy">sudo namp -sU 192.168.0.34</a>
 					</div>
 					<p>
